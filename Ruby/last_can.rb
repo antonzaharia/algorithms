6f2f10172b867ed39b768d59dce6717fe34ba1a3
antonzaharia <zaharia_anton@yahoo.com>
2020-12-29 18:10:44 +0000

    last_can finished

--- Ruby/last_can.rb
+++ Ruby/last_can.rb
@@ -1,4 +1,15 @@
 class LastCan
-    def last_can(array, number)
+    def last_can(array, given_number)
+        last_drink = ""
+        current_number = 1
+        while current_number <= given_number do
+            array.each do |name|
+                if given_number == current_number
+                    last_drink = name
+                end
+                current_number = current_number + 1
+            end
+        end
+        last_drink
     end
 end

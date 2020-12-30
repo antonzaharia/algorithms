e2f58c53a8d2c28dfe156b456223d91ccb97b2b5
antonzaharia <zaharia_anton@yahoo.com>
2020-12-30 17:21:34 +0000

    threeAndFive solved

--- JavaScript/test/threeAndFive.js
+++ JavaScript/test/threeAndFive.js
@@ -1,13 +1,18 @@
 const assert = require("assert");
+const threeAndFive = require("../threeAndFive.js");
 
-describe("threeAndFive", function () {
-  beforeEach(function () {
+describe("Function", function () {
+  it("should return only multiples of 3 or 5", function () {
     const multipleOf3 = threeAndFive(4);
-    const multipleOf5 = threeAndFive(6);
+
+    assert.strictEqual(3, multipleOf3);
   });
-  it("should return only multiples of 3 or 5"),
-    function () {
-      expect(multipleOf3).to.equal(3);
-      expect(multipleOf5).to.equal(14);
-    };
+  it("should return the sum of all multiples of 3 and 5 between 0 and the given number - input given *6*", function () {
+    const sumOfNumbers = threeAndFive(8);
+    assert.strictEqual(14, sumOfNumbers);
+  });
+  it("should return the sum of all multiples of 3 and 5 between 0 and the given number - input given *10*", function () {
+    const sumOfNumbers = threeAndFive(10);
+    assert.strictEqual(23, sumOfNumbers);
+  });
 });
--- JavaScript/threeAndFive.js
+++ JavaScript/threeAndFive.js
@@ -1,3 +1,14 @@
-function threeAndFive() {
-  return true;
+function threeAndFive(number) {
+  let numbers = [];
+  for (let i = 0; i < number; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      numbers.push(i);
+    }
+  }
+  if (numbers.length > 1) {
+    return numbers.reduce((n, memo) => memo + n);
+  } else {
+    return 0;
+  }
 }
+module.exports = threeAndFive;

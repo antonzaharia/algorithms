5e69c58bdf67a8afc45ce4989be7da85ca12f651
antonzaharia <zaharia_anton@yahoo.com>
2021-01-02 14:18:57 +0000

    openOrSenior tests

--- JavaScript/openOrSenior.js
+++ JavaScript/openOrSenior.js
@@ -1,4 +1,4 @@
 function openOrSenior(data) {
-  data;
+  data.map((member) => console.log(member));
 }
 module.exports = openOrSenior;
--- JavaScript/test/openOrSenior.js
+++ JavaScript/test/openOrSenior.js
@@ -2,8 +2,31 @@ const assert = require("assert");
 const openOrSenior = require("../openOrSenior.js");
 
 describe("Function", function () {
-  it("test", function () {
-    const test = openOrSenior("test");
-    assert.strictEqual("data", test);
+  it("should return the correct type of member", function () {
+    const test1 = openOrSenior([
+      [45, 12],
+      [55, 21],
+      [19, -2],
+      [104, 20],
+    ]);
+    assert.strictEqual(["Open", "Senior", "Open", "Senior"], test1);
+  });
+  it("should return the correct type of member 2", function () {
+    const test2 = openOrSenior([
+      [3, 12],
+      [55, 1],
+      [91, -2],
+      [54, 23],
+    ]);
+    assert.strictEqual(["Open", "Open", "Open", "Open"], test2);
+  });
+  it("should return the correct type of member 3", function () {
+    const test3 = openOrSenior([
+      [59, 12],
+      [55, -1],
+      [12, -2],
+      [12, 12],
+    ]);
+    assert.strictEqual(["Senior", "Open", "Open", "Open"], test3);
   });
 });

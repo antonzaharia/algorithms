e72985a571ba6c5780727e815479e0e4d9bcf59f
antonzaharia <zaharia_anton@yahoo.com>
2021-01-03 13:15:57 +0000

    isValidWalk solved

--- JavaScript/isValidWalk.js
+++ JavaScript/isValidWalk.js
@@ -1,4 +1,21 @@
 function isValidWalk(walk) {
-  return walk;
+  let n = 0,
+    s = 0,
+    e = 0,
+    w = 0;
+  walk.map((direction) => {
+    if (direction === "n") {
+      n += 1;
+    } else if (direction === "s") {
+      s += 1;
+    } else if (direction === "e") {
+      e += 1;
+    } else if (direction === "w") {
+      w += 1;
+    }
+  });
+  if (n === s && e === w && walk.length === 10) {
+    return true;
+  } else return false;
 }
 module.exports = isValidWalk;

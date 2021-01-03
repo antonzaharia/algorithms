3dfa38e926dc36740341dcea70a18951657d2b17
antonzaharia <zaharia_anton@yahoo.com>
2021-01-03 13:03:33 +0000

    isValidWalk test

--- JavaScript/test/isValidWalk.js
+++ JavaScript/test/isValidWalk.js
@@ -2,8 +2,33 @@ const assert = require("assert");
 const isValidWalk = require("../isValidWalk.js");
 
 describe("isValidWalk()", function () {
-  it("return true", function () {
-    const test = isValidWalk("test");
-    assert.strictEqual("test", test);
+  it("should return false if array length is smaller than 10 blocks", function () {
+    const test = isValidWalk(["w"]);
+    assert.strictEqual(false, test);
+  });
+  it("should return false if array length is bigger than 10 blocks", function () {
+    const test = isValidWalk([
+      "w",
+      "e",
+      "w",
+      "e",
+      "w",
+      "e",
+      "w",
+      "e",
+      "w",
+      "e",
+      "w",
+      "e",
+    ]);
+    assert.strictEqual(false, test);
+  });
+  it("should return false if walk is not ending in tha same point", function () {
+    test = isValidWalk(["n", "n", "n", "s", "n", "s", "n", "s", "n", "s"]);
+    assert.strictEqual(false, test);
+  });
+  it("should return true if array length equal 10 and ending point is the same as starting point", function () {
+    test = isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]);
+    assert.strictEqual(true, test);
   });
 });

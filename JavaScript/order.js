cab6b07402ac9cbb77e5a02e8e44fbd4bd83f41b
antonzaharia <zaharia_anton@yahoo.com>
2021-01-04 15:58:57 +0000

    order solved

--- JavaScript/order.js
+++ JavaScript/order.js
@@ -1,4 +1,10 @@
 function order(words) {
-  return words;
+  toReturn = [];
+  array = words.split(" ");
+  for (let i = 0; i < array.length; i++) {
+    let word = array.find((word) => word.includes(i + 1));
+    toReturn.push(word);
+  }
+  return toReturn.join(" ");
 }
 module.exports = order;

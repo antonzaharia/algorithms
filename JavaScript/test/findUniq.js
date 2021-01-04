eecbdd61865772146a490598a23acc33c37f8089
antonzaharia <zaharia_anton@yahoo.com>
2021-01-04 16:08:23 +0000

    findUniq tests added

--- JavaScript/test/findUniq.js
+++ JavaScript/test/findUniq.js
@@ -2,8 +2,16 @@ const assert = require("assert");
 const findUniq = require("../findUniq.js");
 
 describe("findUniq()", function () {
-  it("test", function () {
-    test = findUniq("test");
-    assert.strictEqual("test", test);
+  it("should find the uniq different number 1", function () {
+    test = findUniq([0, 1, 0]);
+    assert.strictEqual(1, test);
+  });
+  it("should find the uniq different number 2", function () {
+    test = findUniq([1, 1, 1, 2, 1, 1]);
+    assert.strictEqual(2, test);
+  });
+  it("should find the uniq different number 3", function () {
+    test = findUniq([3, 10, 3, 3, 3]);
+    assert.strictEqual(10, test);
   });
 });

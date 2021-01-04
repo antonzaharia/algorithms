5a990067506270f74a7d90a2dbe6ceee6520a703
antonzaharia <zaharia_anton@yahoo.com>
2021-01-04 16:37:26 +0000

    findUniq solved

--- JavaScript/findUniq.js
+++ JavaScript/findUniq.js
@@ -1,4 +1,11 @@
 function findUniq(arr) {
-  return arr;
+  let toReturn;
+  for (let i = 0; i < arr.length; i++) {
+    newArr = arr.filter((e) => arr.indexOf(e) !== i);
+    if (!newArr.includes(arr[i])) {
+      toReturn = arr[i];
+    }
+  }
+  return toReturn;
 }
 module.exports = findUniq;

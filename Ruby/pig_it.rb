99bd6834acb2a8c31a7db25dfa799eea7ee6515a
antonzaharia <zaharia_anton@yahoo.com>
2021-01-05 14:41:03 +0000

    PitIt solved

--- Ruby/pig_it.rb
+++ Ruby/pig_it.rb
@@ -1,5 +1,15 @@
 class PigIt
     def pig_it(text)
-        text
+        to_return = []
+        text.split(" ").each do |word|
+            if !word.match(/\A\W*\z/)
+                first_letter = word[0]
+                word[0] = ''
+                to_return << "#{word}#{first_letter}ay"
+            else
+                to_return << word
+            end
+        end
+        to_return.join(" ")
     end
 end

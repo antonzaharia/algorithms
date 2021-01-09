899818a8c676314a5832c74c95aa4cab3abf714e
antonzaharia <zaharia_anton@yahoo.com>
2021-01-09 18:55:38 +0000

    tests added

--- Ruby/spec/domain_name_spec.rb
+++ Ruby/spec/domain_name_spec.rb
@@ -1,8 +1,16 @@
 require_relative '../domain_name.rb'
 
 describe DomainName do
-    it('test') do
-        test = DomainName.new.domain_name('test')
-        expect(test).to eq('test')
+    it('should return domain name from simple link') do
+        test = DomainName.new.domain_name("https://www.cnet.com")
+        expect(test).to eq("cnet")
+    end
+    it('should return domain name from londer link') do
+        test = DomainName.new.domain_name("http://www.zombie-bites.com")
+        expect(test).to eq("zombie-bites")
+    end
+    it('should return domain name from any link path') do
+        test = DomainName.new.domain_name("http://www.zombie-bites.com")
+        expect(test).to eq("zombie-bites")
     end
 end

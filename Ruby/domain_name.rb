1de7c90a168ef817cb5b553c8b498fb2898cc517
antonzaharia <zaharia_anton@yahoo.com>
2021-01-09 19:02:54 +0000

    domain_name solved

--- Ruby/domain_name.rb
+++ Ruby/domain_name.rb
@@ -1,5 +1,6 @@
 class DomainName 
   def domain_name(url)
-      url
+      url[0] == 'h' ? output = url.split('//')[1].split(".") : output = url.split('.')
+      output[0] === 'www' ? output[1] : output[0]
   end
 end

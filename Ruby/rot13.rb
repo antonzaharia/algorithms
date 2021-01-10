2d0bf1c005d4a06dd269b2452eb4dfd0021bed6b
antonzaharia <zaharia_anton@yahoo.com>
2021-01-10 18:08:14 +0000

    rot 13 solved

--- Ruby/rot13.rb
+++ Ruby/rot13.rb
@@ -1,5 +1,17 @@
 class Rot13
-    def rot13(message) 
-        message
+    def rot13(message)
+        alphabet = [*'a'..'z']
+        chars = message.split("")
+      
+        shifted = chars.map do |char|
+          if alphabet.include?(char.downcase) && char.capitalize!
+            alphabet.at(alphabet.index(char.downcase) - 13)
+          elsif alphabet.include?(char.downcase)
+            alphabet.at(alphabet.index(char.downcase) - 13).upcase
+          else
+            char
+          end
+        end
+        shifted.join
     end
 end

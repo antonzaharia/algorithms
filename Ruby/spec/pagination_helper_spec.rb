c8b2c8a907ca04b83b07480ef215821e4d441f6b
antonzaharia <zaharia_anton@yahoo.com>
2021-01-11 20:03:41 +0000

    pagination_helper tests

--- Ruby/spec/pagination_helper_spec.rb
+++ Ruby/spec/pagination_helper_spec.rb
@@ -1,8 +1,22 @@
 require_relative '../pagination_helper.rb'
 
 describe PaginationHelper do
-    it 'test' do
-        test = PaginationHelper.new('test', 'test').test('test')
-        expect(test).to eq('test')
+    helper = PaginationHelper.new(['a','b','c','d','e','f'], 4)
+    it 'should count the pages' do
+        expect(helper.page_count).to eq(2)
+    end
+    it 'should count the items' do 
+        expect(helper.item_count).to eq(6)
+    end
+    it 'should count the items for every page' do
+        expect(helper.page_item_count(0)).to eq(4)
+        expect(helper.page_item_count(1)).to eq(2)
+        expect(helper.page_item_count(2)).to eq(-1)
+    end
+    it 'take an item index and return the page that is belongs on' do
+        expect(helper.page_index(5)).to eq(1)
+        expect(helper.page_index(2)).to eq(0)
+        expect(helper.page_index(20)).to eq(-1)
+        expect(helper.page_index(-10)).to eq(-1)
     end
 end

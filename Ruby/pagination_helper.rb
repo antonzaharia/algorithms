9cacf36bcba1d4a530c3e246b8eda8cc98e1eb9d
antonzaharia <zaharia_anton@yahoo.com>
2021-01-11 21:41:46 +0000

    pagination_helper 3 methods solved

--- Ruby/pagination_helper.rb
+++ Ruby/pagination_helper.rb
@@ -1,23 +1,48 @@
-# TODO: complete this class
-
 class PaginationHelper
 
     def initialize(collection, items_per_page)
-  
+        @collection = collection
+        @items_per_page = items_per_page
     end
     
     def item_count
+        @collection.size
     end
       
     def page_count
-
+        if @collection.size % @items_per_page > 0
+            return @collection.size / @items_per_page + 1
+        else
+            return @collection.size / @items_per_page
+        end
     end
       
     def page_item_count(page_index)
-      
+      if page_index < 0
+        return -1
+      else
+        main = []
+        page = 0
+        number_per_page = 0
+        @collection.each do |item|
+            new_item = Hash.new
+            new_item["index"] = @collection.index(item)
+            if number_per_page < @items_per_page
+                number_per_page += 1
+                new_item["page"] = page
+            else
+                number_per_page = 0
+                page += 1
+                new_item["page"] = page
+            end
+            main << new_item
+        end
+        pageHash = main.select { |p| p['page'] == page_index}
+        pageHash.size == 0 ? -1 : pageHash.size
+      end
     end
       
     def page_index(item_index) 
-     
+        
     end
   end

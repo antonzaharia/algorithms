327c1de64afbd1d11504b8bb7d4d9926b44c4e10
antonzaharia <zaharia_anton@yahoo.com>
2021-01-13 13:50:39 +0000

    cakes solved

--- Ruby/cakes.rb
+++ Ruby/cakes.rb
@@ -1,5 +1,9 @@
 class Cakes
     def cakes(recipe, available)
-        test
+        output = []
+        recipe.each do |ingredient, quantity|
+            available[ingredient] ? output << available[ingredient] / quantity : ""
+        end
+        available.size > 0 ? output.min : 0
     end
 end

1ad220f4bb3809999c9f7d72d1da39a190d92e2b
antonzaharia <zaharia_anton@yahoo.com>
2021-01-15 18:25:12 +0000

    detecting any pairs and matching name of hand

--- Ruby/poker_hand.rb
+++ Ruby/poker_hand.rb
@@ -1,7 +1,37 @@
 class PokerHand
   
     def initialize(hand)
-      # ...
+      @hand = hand
+    end
+
+    def define_score(hand)
+        score = 0
+        cards_in_pairs = []
+        pair_one = false
+        pair_two = false
+        three = false
+        four = false
+        full = false
+        hand.split(' ').each do |card|
+            hand.split(' ').each do |c|
+                if card != c && c.split('')[0] == card.split('')[0]
+                    if !cards_in_pairs.include?(card)
+                      cards_in_pairs << card
+                    end
+                end
+            end
+        end
+        if cards_in_pairs.size == 5
+          full = true
+        elsif cards_in_pairs.size == 4 && cards_in_pairs.all? { |c| cards_in_pairs[0].split("")[0] == c.split("")[0]}
+          four = true
+        elsif cards_in_pairs.size == 4
+          pair_two = true
+        elsif cards_in_pairs.size == 3
+          three = true
+        elsif cards_in_pairs.size == 2
+          pair_one = true
+        end
     end
         
     def compare_with(other)

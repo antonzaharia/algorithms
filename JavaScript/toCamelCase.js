acf7f90fd12a53c6b75cadbf435e6997b925ac30
antonzaharia <zaharia_anton@yahoo.com>
2021-01-16 15:17:53 +0000

    toCamelCase solved

--- JavaScript/toCamelCase.js
+++ JavaScript/toCamelCase.js
@@ -1,4 +1,14 @@
 function toCamelCase(str) {
-  return str;
+  arr = str.split(/[.,\_/ -]/);
+  output = arr.map((word) => {
+    if (word.length > 0 && arr.indexOf(word) > 0) {
+      let firstLetter = word.split("")[0];
+      let restOfWord = word.split("").filter((l) => l != firstLetter);
+      return `${firstLetter.toUpperCase()}${restOfWord.join("")}`;
+    } else {
+      return word;
+    }
+  });
+  return output.join("");
 }
 module.exports = toCamelCase;

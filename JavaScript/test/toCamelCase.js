6359365cbe78cd8498066d2682e74ef0f450f8cb
antonzaharia <zaharia_anton@yahoo.com>
2021-01-16 15:15:50 +0000

    toCamelCase tests

--- JavaScript/test/toCamelCase.js
+++ JavaScript/test/toCamelCase.js
@@ -2,8 +2,20 @@ const assert = require("assert");
 const toCamelCase = require("../toCamelCase.js");
 
 describe("toCamelCase()", function () {
-  it("should test", function () {
-    const test = toCamelCase("test");
-    assert.strictEqual("test", test);
+  it("should return an empty string if an empty string is given", function () {
+    const test = toCamelCase("");
+    assert.strictEqual("", test);
+  });
+  it("should remove regex", function () {
+    const test = toCamelCase("A-B-C");
+    assert.strictEqual("ABC", test);
+  });
+  it("should return a camel cased string with first letter lower cased", function () {
+    const test = toCamelCase("the_stealth_warrior");
+    assert.strictEqual("theStealthWarrior", test);
+  });
+  it("should return a camel cased string with first letter upper cased", function () {
+    const test = toCamelCase("The-Stealth-Warrior");
+    assert.strictEqual("TheStealthWarrior", test);
   });
 });

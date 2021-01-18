7a2b82fc5bb524829aca906525988fa268e3154c
antonzaharia <zaharia_anton@yahoo.com>
2021-01-18 21:37:06 +0000

    first_non_repeating_letter solved

--- JavaScript/first_non_repeating_letter.js
+++ JavaScript/first_non_repeating_letter.js
@@ -1,4 +1,20 @@
 function first_non_repeating_letter(str) {
-  return str;
+  const getCount = (arr, letter) => {
+    let count = 0;
+    arr.forEach((l) => l === letter && count++);
+    return count;
+  };
+
+  if (str.length > 0) {
+    output = [];
+    str.split("").map((l) => {
+      if (getCount(str.split(""), l) <= 1) {
+        output.push(l);
+      }
+    });
+    return output[0];
+  } else {
+    return "";
+  }
 }
 module.exports = first_non_repeating_letter;

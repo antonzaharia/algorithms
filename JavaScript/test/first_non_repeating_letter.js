34d82808ada9e30135eb3300f9437b269d6e60ff
antonzaharia <zaharia_anton@yahoo.com>
2021-01-18 21:20:53 +0000

    first_non_repeating_letter tests

--- JavaScript/test/first_non_repeating_letter.js
+++ JavaScript/test/first_non_repeating_letter.js
@@ -2,8 +2,20 @@ const assert = require("assert");
 const first_non_repeating_letter = require("../first_non_repeating_letter.js");
 
 describe("first_non_repeating_letter()", function () {
-  it("test", function () {
-    test = first_non_repeating_letter("test");
-    assert.strictEqual(test, "test");
+  it("should return an empty string if an empty string is passed", function () {
+    test = first_non_repeating_letter("");
+    assert.strictEqual(test, "");
+  });
+  it("should return first letter if none are repeating", function () {
+    test = first_non_repeating_letter("a");
+    assert.strictEqual(test, "a");
+  });
+  it("should return first non repeating letter (only one repeating)", function () {
+    test = first_non_repeating_letter("stress");
+    assert.strictEqual(test, "t");
+  });
+  it("should return first non repeating letter (one not repeating)", function () {
+    test = first_non_repeating_letter("moonmen");
+    assert.strictEqual(test, "e");
   });
 });

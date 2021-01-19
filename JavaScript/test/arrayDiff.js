6ab7a5c7d4a69f9a9ec9e80916c306e948ee2de9
antonzaharia <zaharia_anton@yahoo.com>
2021-01-19 16:01:19 +0000

    arrayDiff tests

--- JavaScript/test/arrayDiff.js
+++ JavaScript/test/arrayDiff.js
@@ -2,8 +2,20 @@ const assert = require("assert");
 const arrayDiff = require("../arrayDiff");
 
 describe("arrayDiff()", function () {
-  it("test", function () {
-    test = arrayDiff("test", "test");
-    assert.strictEqual("test", test);
+  it("should return first array if second array is empty", function () {
+    test = arrayDiff([1, 2], []);
+    assert.strictEqual([1, 2], test);
+  });
+  it("should return an empty array", function () {
+    test = arrayDiff([], [4, 5, 6]);
+    assert.strictEqual([1, 2], test);
+  });
+  it("should return only the elements that are not present in second array", function () {
+    test = arrayDiff([3, 4], [3]);
+    assert.strictEqual([4], test);
+  });
+  it("should return the elements from first array that are not present in second array", function () {
+    test = arrayDiff([1, 2, 2, 3, 2, 4, 5, 3, 6], [2]);
+    assert.strictEqual([1, 3, 4, 5, 6], test);
   });
 });

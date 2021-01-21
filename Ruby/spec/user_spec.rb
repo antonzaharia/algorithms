d17af398cec139824809efaed2b7d6457a58c5ea
antonzaharia <zaharia_anton@yahoo.com>
2021-01-21 18:45:40 +0000

    User Rank solved for basics tests

--- Ruby/spec/user_spec.rb
+++ Ruby/spec/user_spec.rb
@@ -1,15 +1,22 @@
 require_relative '../user.rb'
 describe User do
     it 'should initiate any user with rank -8' do
-        test = User.new
-        expect(test.rank).to eq(-8)
+        user = User.new
+        expect(user.rank).to eq(-8)
+    end
+    it 'should initiate any user with progress 0' do
+        user = User.new
+        expect(user.progress).to eq(0)
     end
     it 'should correctly update the progress' do
+        user = User.new
         user.inc_progress(-7)
-        expect(test.progress).to eq(10)
+        expect(user.progress).to eq(10)
     end
     it 'should correctly update the rank' do
+        user = User.new
+        user.inc_progress(-7)
         user.inc_progress(-5)
-        expect(test.rank).to eq(-7)
+        expect(user.rank).to eq(-7)
     end
 end
--- Ruby/user.rb
+++ Ruby/user.rb
@@ -1,12 +1,41 @@
 class User
+
+    RANK_ARR = [-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8]
+    PROGRESS_ARR = (0..100).to_a
+
+    def initialize
+        @rank = RANK_ARR[0]
+        @progress = PROGRESS_ARR[0]
+    end
     
     def rank
-        
+        @rank
     end
 
     def progress
+        @progress
     end
 
-    def inc_progress
+    def inc_progress(action_rank)
+        d = RANK_ARR.find_index(@rank) - RANK_ARR.find_index(action_rank)
+        progress_build = 10 * d * d
+        set_progress(progress_build)
+    end
+
+    private
+    def set_progress(progress_build)
+        total = @progress + progress_build
+        while total >= 100 do
+            rank_up
+            total = total -100
+        end
+        @progress = total
+    end
+    def rank_up
+        if @rank <= 8
+            @rank = RANK_ARR[RANK_ARR.find_index(@rank) + 1]
+        else
+            puts "Top Rank!"
+        end
     end
 end

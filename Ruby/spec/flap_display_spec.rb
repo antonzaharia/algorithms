47128c79cc5bd4b826c40a33348af848d62ede58
antonzaharia <zaharia_anton@yahoo.com>
2021-01-22 18:26:23 +0000

    basic tests added

--- Ruby/spec/flap_display_spec.rb
+++ Ruby/spec/flap_display_spec.rb
@@ -1,8 +1,16 @@
 require_relative '../flap_display'
 
 describe FlapDisplay do
-    it 'test' do
-        test = FlapDisplay.new.flap_display('test', 'test')
-        expect(test).to eq('test')
+    it 'should should return an empty string if nothing is passed' do
+        test = FlapDisplay.new.flap_display([""],[[]])
+        expect(test).to eq([""])
+    end
+    it 'should not change anything if rotors passed are 0' do
+        test = FlapDisplay.new.flap_display(["NOTHING MOVED"],[[0,0,0,0,0,0,0,0,0,0,0,0,0]])
+        expect(test).to eq(["NOTHING MOVED"])
+    end
+    it 'should correctly change the letters' do
+        test = FlapDisplay.new.flap_display(["CAT"],[[1, 13, 27]])
+        expect(test).to eq(["DOG"])
     end
 end

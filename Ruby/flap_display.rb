a7ec546cd3d024c198aca1bdb62e3c9329c1b15b
antonzaharia <zaharia_anton@yahoo.com>
2021-01-23 13:38:17 +0000

    Flap Display set_index method

--- Ruby/flap_display.rb
+++ Ruby/flap_display.rb
@@ -1,13 +1,22 @@
 class FlapDisplay 
-    ABC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "?", "!", "@", "#", "&", "(", ")", "|", "<", ">", ".", ":", "=", "-", "+", "*", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    ABC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     def flap_display(lines, rotors)
         output = []
         if lines.size > 0
-            lines[0].split("").each_with_index do |line, i|
-                given_index = ABC.find_index(rotors[0][i])
-                output << ABC[given_index + line]
+            rotors[0].each_with_index do |rotor, i|
+              line_arr = lines[0].split("")[i]
+                given_index = ABC.find_index(line_arr)
+                output << ABC[set_index(given_index, rotor)]
             end
         end
         output
+    end
+    
+    def set_index(given_index, rotor)
+      output = given_index + rotor
+      while output > 26 do
+        output = output - 26
+      end
+      output
     end
 end

1bec785af9d176bd5119beae2d8938389bd14310
antonzaharia <zaharia_anton@yahoo.com>
2021-01-23 13:37:02 +0000

    Flap display basic change solved

--- Ruby/flap_display.rb
+++ Ruby/flap_display.rb
@@ -1,5 +1,13 @@
 class FlapDisplay 
+    ABC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "?", "!", "@", "#", "&", "(", ")", "|", "<", ">", ".", ":", "=", "-", "+", "*", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     def flap_display(lines, rotors)
-        lines
+        output = []
+        if lines.size > 0
+            lines[0].split("").each_with_index do |line, i|
+                given_index = ABC.find_index(rotors[0][i])
+                output << ABC[given_index + line]
+            end
+        end
+        output
     end
 end

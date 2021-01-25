e93e080fe3db6b3587d450468977ea5414a43ccd
antonzaharia <zaharia_anton@yahoo.com>
2021-01-25 16:56:03 +0000

    Next Bigger Solved

--- Ruby/next_bigger.rb
+++ Ruby/next_bigger.rb
@@ -1,5 +1,27 @@
 class NextBigger
   def next_bigger(n)
-    n
+    if length(n) > 1
+        reverse_last(n)
+    else
+        -1
+    end
+  end
+
+  def length(int)
+    Math.log10(int).to_i + 1
+  end
+
+  def reverse_last(int)
+    arr = int.to_s.split('')
+    count = 1
+    last = arr[arr.size - count]
+    before_last = arr[arr.size - (count + 1)]
+    arr[arr.size - count] = before_last
+    arr[arr.size - (count + 1)] = last
+    if arr.join("").to_i > int
+        arr.join("").to_i
+    else
+        -1
+    end
   end
 end

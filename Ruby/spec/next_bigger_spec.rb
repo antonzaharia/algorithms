b657c664e1c8b9e55af3ca72f6d45e1d40b06128
antonzaharia <zaharia_anton@yahoo.com>
2021-01-25 16:34:12 +0000

    NextBigger tests

--- Ruby/spec/next_bigger_spec.rb
+++ Ruby/spec/next_bigger_spec.rb
@@ -1,8 +1,16 @@
 require_relative '../next_bigger.rb'
 
 describe NextBigger do
-    it 'test' do
-        test = NextBigger.new.next_bigger('test')
-        expect(test).to eq('test')
+    it 'should return -1 if there is not a next bigger number' do
+        test = NextBigger.new.next_bigger(9)
+        expect(test).to eq(-1)
+    end
+    it 'should return the next bigger number of 2 digit numbers' do
+        test = NextBigger.new.next_bigger(12)
+        expect(test).to eq(21)
+    end
+    it 'should return the next bigger number of 3 digit numbers' do
+        test = NextBigger.new.next_bigger(513)
+        expect(test).to eq(531)
     end
 end

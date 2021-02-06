3a00b8d06f0d088215bbd80d4117168eb4849aa0
antonzaharia <zaharia_anton@yahoo.com>
2021-02-06 14:24:38 +0000

    findOutlier solved

--- JavaScript/findOutlier.js
+++ JavaScript/findOutlier.js
@@ -1,4 +1,17 @@
 function findOutlier(integers) {
-  return integers;
+  let even = [];
+  let odd = [];
+  integers.map((integer) => {
+    if (integer % 2 === 0) {
+      even.push(integer);
+    } else {
+      odd.push(integer);
+    }
+  });
+  if (even.length === 1) {
+    return even[0];
+  } else {
+    return odd[0];
+  }
 }
 module.exports = findOutlier;

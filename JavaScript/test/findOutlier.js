ae14a976227adecc975d40b9a0d38322a4645c4d
antonzaharia <zaharia_anton@yahoo.com>
2021-02-06 14:13:46 +0000

    findOutlier tests basic

--- JavaScript/test/findOutlier.js
+++ JavaScript/test/findOutlier.js
@@ -2,8 +2,12 @@ const assert = require("assert");
 const findOutlier = require("../findOutlier");
 
 describe("findOutlier()", function () {
-  it("test", function () {
-    let test = findOutlier("test");
-    assert.strictEqual(test, "test");
+  it("should find any odd number from even numbers", function () {
+    let test = findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]);
+    assert.strictEqual(test, 11);
+  });
+  it("should find any even number from odd numbers", function () {
+    let test = findOutlier([160, 3, 1719, 19, 11, 13, -21]);
+    assert.strictEqual(test, 160);
   });
 });

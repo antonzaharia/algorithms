f7beb6abbf2a11ca5a32e26e012d6398c57cc26d
antonzaharia <zaharia_anton@yahoo.com>
2021-02-07 15:31:31 +0000

    tickets() basic tests added

--- JavaScript/test/tickets.js
+++ JavaScript/test/tickets.js
@@ -2,8 +2,12 @@ const assert = require("assert");
 const tickets = require("../tickets.js");
 
 describe("tickets()", function () {
-  it("test", function () {
-    let test = tickets("test");
-    assert(test, "test");
+  it("should test if no change needed", function () {
+    let test = tickets([25]);
+    assert(test, "YES");
+  });
+  it("should return NO if change is needed at first sale", function () {
+    let test = tickets([50]);
+    assert(test, "NO");
   });
 });

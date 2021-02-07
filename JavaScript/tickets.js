e1393f6431d084bcee7f2080cc38436684981859
antonzaharia <zaharia_anton@yahoo.com>
2021-02-07 15:45:48 +0000

    tickets() solved

--- JavaScript/tickets.js
+++ JavaScript/tickets.js
@@ -1,3 +1,20 @@
-function tickets(peopleInLine) {}
+function tickets(peopleInLine) {
+  let till = 0;
+  const ticket = 25;
+  let problem = false;
+  peopleInLine.map((cash) => {
+    let changeNeeded = cash - ticket;
+    if (till - changeNeeded < 0) {
+      problem = true;
+    } else {
+      till = till + 25 - changeNeeded;
+    }
+  });
+  if (problem) {
+    return "NO";
+  } else {
+    return "YES";
+  }
+}
 
 module.exports = tickets;

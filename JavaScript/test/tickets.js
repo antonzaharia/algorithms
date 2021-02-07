1bb85ad8d3f6f18fd846bbfd492de5f17a0bff32
antonzaharia <zaharia_anton@yahoo.com>
2021-02-07 15:34:37 +0000

    strict equal error fixed && complex tests added

--- JavaScript/test/tickets.js
+++ JavaScript/test/tickets.js
@@ -4,10 +4,18 @@ const tickets = require("../tickets.js");
 describe("tickets()", function () {
   it("should test if no change needed", function () {
     let test = tickets([25]);
-    assert(test, "YES");
+    assert.strictEqual(test, "YES");
   });
   it("should return NO if change is needed at first sale", function () {
     let test = tickets([50]);
-    assert(test, "NO");
+    assert.strictEqual(test, "NO");
+  });
+  it("should return YES if enough change is available", function () {
+    let test = tickets([25, 25, 50, 50]);
+    assert.strictEqual(test, "NO");
+  });
+  it("should return NO if not enough change is available", function () {
+    let test = tickets([25, 25, 50, 50, 100]);
+    assert.strictEqual(test, "NO");
   });
 });

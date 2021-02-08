5aee18f01406a6c66a9761033bf456a4943917a0
antonzaharia <zaharia_anton@yahoo.com>
2021-02-08 15:28:58 +0000

    cameCase() prototype solved basic tests

--- JavaScript/camelCase.js
+++ JavaScript/camelCase.js
@@ -1,3 +1,17 @@
 String.prototype.camelCase = function () {
-  return Array.from(this).join("");
+  const word = Array.from(this);
+  output = [];
+  for (let i = 0; i < word.length; i++) {
+    if (i === 0) {
+      output.push(word[i].toUpperCase());
+    } else {
+      if (word[i] === " " && i !== word.length) {
+        output.push(word[i + 1].toUpperCase());
+        i++;
+      } else {
+        output.push(word[i]);
+      }
+    }
+  }
+  return output.join("");
 };

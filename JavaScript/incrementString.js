24d435866c52867a03c32ac0d757cdf016cd3b04
antonzaharia <zaharia_anton@yahoo.com>
2021-02-09 19:37:54 +0000

    incrementString() solved

--- JavaScript/incrementString.js
+++ JavaScript/incrementString.js
@@ -1,4 +1,20 @@
 function incrementString(string) {
-  return string;
+  const array = string.split(/(?=\d+)/);
+  let s = array.find((e) => typeof e === "string" || e instanceof String);
+  let numArray = array.filter((e) => e !== s);
+
+  let zeros = [];
+  let index = 0;
+  for (let i = 0; i < numArray.length; i++) {
+    if (parseInt(numArray[i]) === 0) {
+      index = i;
+      zeros.push("0");
+    } else {
+      index = i;
+      break;
+    }
+  }
+  let num = parseInt(numArray.splice(index, numArray.length).join("")) + 1;
+  return `${s}${zeros.length > 0 ? zeros.join("") : ""}${num ? num : 1}`;
 }
 module.exports = incrementString;

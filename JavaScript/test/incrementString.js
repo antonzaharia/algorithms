e702752331bd326be28536f0bff274d37f25784e
antonzaharia <zaharia_anton@yahoo.com>
2021-02-09 17:32:07 +0000

    incrementString() tests added

--- JavaScript/test/incrementString.js
+++ JavaScript/test/incrementString.js
@@ -2,8 +2,20 @@ const assert = require("assert");
 const incrementString = require("../incrementString");
 
 describe("incrementString()", () => {
-  it("test", () => {
-    let test = incrementString("test");
-    assert.strictEqual(test, "test");
+  it("should add the integer at the end of the string", () => {
+    let test = incrementString("foo");
+    assert.strictEqual(test, "foo1");
+  });
+  it("should add the integer at the end of the string", () => {
+    let test = incrementString("");
+    assert.strictEqual(test, "1");
+  });
+  it("should correctly increment the number", () => {
+    let test = incrementString("foobar001");
+    assert.strictEqual(test, "foobar002");
+  });
+  it("should correctly increment the number", () => {
+    let test = incrementString("foobar99");
+    assert.strictEqual(test, "foobar100");
   });
 });

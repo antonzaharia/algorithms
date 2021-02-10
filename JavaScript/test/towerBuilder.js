31d1222499602c90a4831187985d1352a7bf1a7e
antonzaharia <zaharia_anton@yahoo.com>
2021-02-10 17:50:58 +0000

    towerBuilder() solved

--- JavaScript/test/towerBuilder.js
+++ JavaScript/test/towerBuilder.js
@@ -4,14 +4,14 @@ const towerBuilder = require("../towerBuilder");
 describe("towerBuilder()", () => {
   it("should correctly build a tower with one floor", () => {
     let test = towerBuilder(1);
-    assert.strictEqual(test, ["*"]);
+    assert.deepStrictEqual(test, ["*"]);
   });
   it("should correctly build a tower with two floors", () => {
     let test1 = towerBuilder(2);
-    assert.strictEqual(test1, [" * ", "***"]);
+    assert.deepStrictEqual(test1, [" * ", "***"]);
   });
   it("should correctly build a tower with three floors", () => {
     let test2 = towerBuilder(3);
-    assert.strictEqual(test2, ["  *  ", " *** ", "*****"]);
+    assert.deepStrictEqual(test2, ["  *  ", " *** ", "*****"]);
   });
 });
--- JavaScript/towerBuilder.js
+++ JavaScript/towerBuilder.js
@@ -1,4 +1,12 @@
 function towerBuilder(nFloors) {
-  return nFloors;
+  let output = [];
+  const setNumberOfStars = (index) => index * 2 + 1;
+
+  for (let i = 0; i < nFloors; i++) {
+    let stars = setNumberOfStars(i);
+    let space = (setNumberOfStars(nFloors - 1) - stars) / 2;
+    output.push(`${" ".repeat(space)}${"*".repeat(stars)}${" ".repeat(space)}`);
+  }
+  return output;
 }
 module.exports = towerBuilder;

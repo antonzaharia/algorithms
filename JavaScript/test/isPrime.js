dc30d9a3a4575d43f076d18966b4e30b6359149b
antonzaharia <zaharia_anton@yahoo.com>
2021-02-11 18:54:48 +0000

    isPrime() tests added

--- JavaScript/test/isPrime.js
+++ JavaScript/test/isPrime.js
@@ -2,8 +2,24 @@ const assert = require("assert");
 const isPrime = require("../isPrime");
 
 describe("isPrime()", () => {
-  it("test", () => {
-    let test = isPrime("test");
-    assert.strictEqual(test, "test");
+  it("should return false as 1 is not prime", () => {
+    let test = isPrime(1);
+    assert.strictEqual(test, false);
+  });
+  it("should return true as 2 is prime", () => {
+    let test = isPrime(2);
+    assert.strictEqual(test, true);
+  });
+  it("should return false as 75 is not prime", () => {
+    let test = isPrime(75);
+    assert.strictEqual(test, false);
+  });
+  it("should return false as -1 is not prime", () => {
+    let test = isPrime(-1);
+    assert.strictEqual(test, false);
+  });
+  it("should return true as 73 is prime", () => {
+    let test = isPrime(73);
+    assert.strictEqual(test, true);
   });
 });

3b057a54851613b8d26883f8c4ddd0cfa8cef06e
antonzaharia <zaharia_anton@yahoo.com>
2021-02-12 16:07:11 +0000

    goodVsEvil() tests added

--- JavaScript/test/goodVsEvil.js
+++ JavaScript/test/goodVsEvil.js
@@ -2,8 +2,22 @@ const assert = require("assert");
 const goodVsEvil = require("../goodVsEvil.js");
 
 describe("goodVsEvil()", () => {
-  it("test", () => {
-    let test = goodVsEvil("test");
-    assert.strictEqual(test, "test");
+  it("evil should win", () => {
+    let test = goodVsEvil("1 1 1 1 1 1", "1 1 1 1 1 1 1");
+    assert.strictEqual(
+      test,
+      "Battle Result: Evil eradicates all trace of Good"
+    );
+  });
+  it("good should win", () => {
+    let test = goodVsEvil("0 0 0 0 0 10", "0 1 1 1 1 0 0");
+    assert.strictEqual(
+      test,
+      "Battle Result: Good triumphs over Evil', 'Good should win"
+    );
+  });
+  it("should be a tie", () => {
+    let test = goodVsEvil("1 0 0 0 0 0", "1 0 0 0 0 0 0");
+    assert.strictEqual(test, "Battle Result: No victor on this battle field");
   });
 });

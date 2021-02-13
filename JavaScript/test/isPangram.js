c72aea09ffb526ec73ced9de96254c7181e4a5d9
antonzaharia <zaharia_anton@yahoo.com>
2021-02-13 15:47:10 +0000

    isPangram() tests added

--- JavaScript/test/isPangram.js
+++ JavaScript/test/isPangram.js
@@ -2,8 +2,12 @@ const assert = require("assert");
 const isPangram = require("../isPangram");
 
 describe("isPangram()", () => {
-  it("test", () => {
-    let test = isPangram("test");
-    assert.strictEqual(test, "test");
+  it("should return true if the string is a pangram", () => {
+    let test = isPangram("The quick brown fox jumps over the lazy dog.");
+    assert.strictEqual(test, true);
+  });
+  it("should return false if the string is not a pangram", () => {
+    let test = isPangram("This is not a pangram.");
+    assert.strictEqual(test, false);
   });
 });

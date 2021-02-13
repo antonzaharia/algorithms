6dd8f1332562908bd7e9687b9bfaae92ae3a0625
antonzaharia <zaharia_anton@yahoo.com>
2021-02-13 15:41:27 +0000

    createPhoneNumber() solved

--- JavaScript/createPhoneNumber.js
+++ JavaScript/createPhoneNumber.js
@@ -1,4 +1,16 @@
 function createPhoneNumber(numbers) {
-  return numbers;
+  let output = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (output.length === 0) {
+      output.push("(");
+    } else if (output.length === 4) {
+      output.push(") ");
+    } else if (output.length === 8) {
+      output.push("-");
+    }
+    output.push(numbers[i]);
+  }
+  return output.join("");
 }
 module.exports = createPhoneNumber;

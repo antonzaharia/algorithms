0bcea7ed1b15781c7c3dc99195ad0df6cee2dbc4
antonzaharia <zaharia_anton@yahoo.com>
2021-02-14 12:54:40 +0000

    simpleEncryption tests added

--- JavaScript/test/simpleEncryption.js
+++ JavaScript/test/simpleEncryption.js
@@ -3,14 +3,38 @@ const encrypt = require("../simpleEncryption");
 const decrypt = require("../simpleEncryption");
 
 describe("encrypt()", () => {
-  it("encrypt()", () => {
-    let test = encrypt("text");
-    assert(test, "text");
+  it("should return text input", () => {
+    let test = encrypt("This is a test!", 0);
+    assert.strictEqual(test, "This is a test!");
+  });
+  it("should return text input encrypted once", () => {
+    let test = encrypt("This is a test!", 0);
+    assert.strictEqual(test, "hsi  etTi sats!");
+  });
+  it("should return text input encrypted twice", () => {
+    let test = encrypt("This is a test!", 2);
+    assert.strictEqual(test, "s eT ashi tist!");
+  });
+  it("should return text input encrypted three times", () => {
+    let test = encrypt("This is a test!", 3);
+    assert.strictEqual(test, " Tah itse sits!");
   });
 });
 describe("decrypt()", () => {
-  it("decrypt()", () => {
-    let test = decrypt("text");
-    assert(test, "text");
+  it("should return text input", () => {
+    let test = decrypt("This is a test!", 0);
+    assert.strictEqual(test, "This is a test!");
+  });
+  it("should return decrypted test once", () => {
+    let test = decrypt("hsi  etTi sats!", 1);
+    assert.strictEqual(test, "This is a test!");
+  });
+  it("should return decrypted test twice", () => {
+    let test = decrypt("s eT ashi tist!", 2);
+    assert.strictEqual(test, "This is a test!");
+  });
+  it("should return decrypted test three times", () => {
+    let test = decrypt(" Tah itse sits!", 3);
+    assert.strictEqual(test, "This is a test!");
   });
 });

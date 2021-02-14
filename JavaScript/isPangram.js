4e3b6f21622b15c5a7747ac9f71e73abea1ed9da
antonzaharia <zaharia_anton@yahoo.com>
2021-02-14 12:31:06 +0000

    isPangram() solved

--- JavaScript/isPangram.js
+++ JavaScript/isPangram.js
@@ -1,4 +1,13 @@
 function isPangram(string) {
-  return string;
+  const abc = "abcdefghijklmnopqrstuvwxyz".split("");
+  let arrOfArrays = string.split(" ").map((word) => word.split(""));
+  let arrayOfLetters = [].concat.apply([], arrOfArrays);
+  let result = true;
+  for (let i = 0; i < abc.length; i++) {
+    if (!arrayOfLetters.find((l) => abc[i] === l.toLowerCase())) {
+      result = false;
+    }
+  }
+  return result;
 }
 module.exports = isPangram;

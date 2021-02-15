1379d1e424475e3c3fc30752783f50932dc55b74
antonzaharia <zaharia_anton@yahoo.com>
2021-02-15 16:41:42 +0000

    formatDuration() tests added

--- JavaScript/test/formatDuration.js
+++ JavaScript/test/formatDuration.js
@@ -2,8 +2,24 @@ const assert = require("assert");
 const formatDuration = require("../formatDuration.js");
 
 describe("formatDuration()", () => {
-  it("test", () => {
-    let test = formatDuration("test");
-    assert.strictEqual(test, "test");
+  it("should return seconds if less than a minute", () => {
+    let test = formatDuration(1);
+    assert.strictEqual(test, "1 second");
+  });
+  it("should return minutes and seconds if more than a minute", () => {
+    let test = formatDuration(62);
+    assert.strictEqual(test, "1 minute and 2 seconds");
+  });
+  it("should return only minutes if 0 extra seconds", () => {
+    let test = formatDuration(120);
+    assert.strictEqual(test, "2 minutes");
+  });
+  it("should return only hours if 60 minutes", () => {
+    let test = formatDuration(3600);
+    assert.strictEqual(test, "1 hour");
+  });
+  it("should return hours minutes and seconds", () => {
+    let test = formatDuration(3662);
+    assert.strictEqual(test, "1 hour, 1 minute and 2 seconds");
   });
 });

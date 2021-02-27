34a2aeb7bac1bba2bb5b9d34764b67c62b4372fa
antonzaharia <zaharia_anton@yahoo.com>
2021-02-27 12:44:17 +0000

    strings add tests added

--- JavaScript/test/add.js
+++ JavaScript/test/add.js
@@ -2,8 +2,19 @@ const assert = require("assert");
 const add = require("../add");
 
 describe("add()", () => {
-  it("test", () => {
-    let test = add("test");
-    assert.strictEqual(test, "test");
+  it("should return the sum of two strings", () => {
+    let test = add("1", "1");
+    assert.strictEqual(test, "2");
+  });
+  it("should return the sum of two strings 2", () => {
+    let test = add("123", "456");
+    assert.strictEqual(test, "579");
+  });
+  it("should return the sum of two strings 3", () => {
+    let test = add(
+      "63829983432984289347293874",
+      "90938498237058927340892374089"
+    );
+    assert.strictEqual(test, "91002328220491911630239667963");
   });
 });

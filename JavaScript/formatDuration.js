1dbcd2dd1078374ebcf8ed524a6c3f11c5dd3caa
antonzaharia <zaharia_anton@yahoo.com>
2021-02-27 13:05:54 +0000

    formatDuration count solved part1

--- JavaScript/formatDuration.js
+++ JavaScript/formatDuration.js
@@ -1,5 +1,19 @@
 function formatDuration(seconds) {
-  return seconds;
+  let minutes = 0;
+  let hours = 0;
+  let s = 0;
+  for (let i = 0; i < seconds; i++) {
+    s += 1;
+    if (s === 60) {
+      s = s - 60;
+      minutes += 1;
+    }
+    if (minutes === 60) {
+      minutes = minutes - 60;
+      hours += 1;
+    }
+  }
+  return `${hours} hours ${minutes} minutes ${s} seconds`;
 }
 
 module.exports = formatDuration;

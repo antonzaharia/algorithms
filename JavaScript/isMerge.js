2056add968abe125c72f262f2d0e206d6364f124
antonzaharia <zaharia_anton@yahoo.com>
2021-02-28 17:49:56 +0000

    isMerge() solved

--- JavaScript/isMerge.js
+++ JavaScript/isMerge.js
@@ -1,4 +1,23 @@
 function isMerge(s, part1, part2) {
-  return false;
+  const complete = s.split("");
+  p1 = [...part1];
+  p2 = [...part2];
+  let compose = [];
+  for (let i = 0; i < complete.length; i++) {
+    if (complete[i] === p1[0]) {
+      p1.shift();
+      compose.push(complete[i]);
+    } else if (complete[i] === p2[0]) {
+      p2.shift();
+      compose.push(complete[i]);
+    } else {
+      return false;
+    }
+  }
+  if (compose.join("") === s) {
+    return true;
+  } else {
+    return false;
+  }
 }
 module.exports = isMerge;

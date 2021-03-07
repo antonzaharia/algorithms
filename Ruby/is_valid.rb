774ae7fb292518c065339473201428a91f476ee8
antonzaharia <zaharia_anton@yahoo.com>
2021-03-07 09:38:16 +0000

    isValidISBN solved

--- Ruby/is_valid.rb
+++ Ruby/is_valid.rb
@@ -1,5 +1,21 @@
 class IsValid
     def validISBN10(isbn)
-        isbn
+        total = 0
+        output = true
+  
+        return false if isbn.size != 10
+  
+        isbn.split("").each_with_index do |number, index|
+        if number == "X" && index == 9
+            number = 10
+         end
+        if ("A".."Z").include?(number)
+         output = false
+         else 
+          total = total + (number.to_i * (index + 1))
+         end
+        end
+  
+        output && (total % 11) == 0 
     end
 end

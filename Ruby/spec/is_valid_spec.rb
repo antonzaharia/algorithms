b1039c1fc9373af050f7f18e54ae9cdd24a1dff3
antonzaharia <zaharia_anton@yahoo.com>
2021-03-07 09:33:15 +0000

    isValidISBN basic tests

--- Ruby/spec/is_valid_spec.rb
+++ Ruby/spec/is_valid_spec.rb
@@ -1,8 +1,17 @@
 require_relative('../is_valid.rb')
 
 describe IsValid do
-    it "test" do
-        test = IsValid.new.validISBN10('test')
-        expect(test).to eq('test')
+    it "should return true if is a valid ISBN" do
+        test = IsValid.new.validISBN10('1112223339')
+        expect(test).to eq(true)
     end
+    it "should return false if is not a valid ISBN" do
+        test = IsValid.new.validISBN10('1234512345')
+        expect(test).to eq(false)
+    end
+    it "should return false if there are not 10 chars" do
+        test = IsValid.new.validISBN10('111222333')
+        expect(test).to eq(false)
+    end
+    
 end

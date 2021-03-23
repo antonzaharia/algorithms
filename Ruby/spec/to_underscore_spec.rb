18d9448e6d898d2ed09a4e7a83b0e6506e2318c3
antonzaharia <zaharia_anton@yahoo.com>
2021-03-23 17:18:52 +0000

    to_underscore tests

--- Ruby/spec/to_underscore_spec.rb
+++ Ruby/spec/to_underscore_spec.rb
@@ -1,8 +1,16 @@
 require_relative '../to_underscore'
 
 describe ToUnderscore do
-    it "test" do
-        test = ToUnderscore.new.to_underscore('test')
+    it "should return a lowercase word" do
+        test = ToUnderscore.new.to_underscore('Test')
         expect(test).to eq('test')
+    end
+    it "should convert camel cased string into snake case notation" do
+        test = ToUnderscore.new.to_underscore('TestController')
+        expect(test).to eq('test_controller')
+    end
+    it "should convert camel cased string into snake case notation and ignore numbers" do
+        test = ToUnderscore.new.to_underscore('App7Test')
+        expect(test).to eq('app7_test')
     end
 end

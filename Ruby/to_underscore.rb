43ecee6081e6dcd5e4d73c335f5ca01316185344
antonzaharia <zaharia_anton@yahoo.com>
2021-03-23 18:33:49 +0000

    to_underscore 2 tests passed

--- Ruby/to_underscore.rb
+++ Ruby/to_underscore.rb
@@ -1,5 +1,18 @@
 class ToUnderscore
-    def to_underscore(test)
-        test
+    def to_underscore(string)
+        array = []
+        last_index = 0
+        string.split("").each_with_index do |letter, i|
+            if letter == letter.upcase
+                if array.size == 0
+                    array << letter.downcase
+                else
+                    array << "_#{letter.downcase}" 
+                end
+            else
+                array << letter
+            end
+        end
+        array.join("")
     end
 end

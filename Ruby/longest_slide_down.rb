a5103400904b4d5f3ad3ba8330ca18c0d0f1698c
antonzaharia <zaharia_anton@yahoo.com>
2021-03-24 15:53:46 +0000

    Pyramid longest_slide_down solved

--- Ruby/longest_slide_down.rb
+++ Ruby/longest_slide_down.rb
@@ -3,6 +3,10 @@ class Pyramid
         @pyramid = pyramid
     end
     def longest_slide_down
-        @pyramid
+        very_last_row = @pyramid.pop
+        @pyramid.reverse.inject(very_last_row) do |last_row, row|
+           best_choices = last_row.each_cons(2).map(&:max)
+           row.zip(best_choices).map { |x| x.inject(:+) }
+        end[0]
     end
 end

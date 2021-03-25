bb9367709e5f82b19fd1f2c8221f5bb28db2475c
antonzaharia <zaharia_anton@yahoo.com>
2021-03-25 15:08:13 +0000

    Scramble tests

--- Ruby/spec/scramble_spec.rb
+++ Ruby/spec/scramble_spec.rb
@@ -1,8 +1,16 @@
 require_relative '../scramble.rb'
 
 describe "Scramble" do
-    it "test" do
-        test = Scramble.new("test", "test2").play
-        expect(test).to eq('test')
+    it "return true if a portion of str1 characters can be rearranged to match str2" do
+        test = Scramble.new('rkqodlw', 'world').play
+        expect(test).to eq(true)
+    end
+    it "return true if a portion of str1 characters can be rearranged to match str2 - longer word" do
+        test = Scramble.new('cedewaraaossoqqyt', 'codewars').play
+        expect(test).to eq(true)
+    end
+    it "return false if str1 characters cannot be rearranged to match str2" do
+        test = Scramble.new('katas', 'steak').play
+        expect(test).to eq(false)
     end
 end
